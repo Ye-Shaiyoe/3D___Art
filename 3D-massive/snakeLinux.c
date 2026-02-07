cfb317819a00ed7cc3df80c28dd791a082e85998
Akram <akrommuhammadyusuf@gmail.com>
2026-02-07 14:11:01 +0700

    Update snakeLinux.c

--- 3D-massive/snakeLinux.c
+++ 3D-massive/snakeLinux.c
@@ -110,3 +110,172 @@ void clearScreen() {
     printf("\033[2J");
     printf("\033[H");
 }
+
+
+void drawUI(Game *game) {
+    setColor(33); // Yellow
+    gotoxy(WIDTH + 2, 2);
+    printf("🎮 SNAKE GAME 🎮");
+    
+    setColor(32); // Green
+    gotoxy(WIDTH + 2, 4);
+    printf("Score: %d  ", game->score);
+    
+    gotoxy(WIDTH + 2, 5);
+    printf("Length: %d  ", game->snake.length);
+    
+    setColor(35); // Magenta
+    gotoxy(WIDTH + 2, 7);
+    printf("Controls:");
+    setColor(37); // White
+    gotoxy(WIDTH + 2, 8);
+    printf("W - Up");
+    gotoxy(WIDTH + 2, 9);
+    printf("S - Down");
+    gotoxy(WIDTH + 2, 10);
+    printf("A - Left");
+    gotoxy(WIDTH + 2, 11);
+    printf("D - Right");
+    gotoxy(WIDTH + 2, 12);
+    printf("Q - Exit");
+}
+
+void drawSnake(Snake *snake) {
+    setColor(33); // Yellow
+    gotoxy(snake->x[0], snake->y[0]);
+    printf("◉");
+    
+    setColor(32); // Green
+    for (int i = 1; i < snake->length; i++) {
+        gotoxy(snake->x[i], snake->y[i]);
+        printf("●");
+    }
+}
+
+void drawFood(Food *food) {
+    setColor(31); // Red
+    gotoxy(food->x, food->y);
+    printf("★");
+}
+
+void handleInput(Game *game) {
+    if (kbhit()) {
+        char key = getchar();
+        switch (key) {
+            case 'w': case 'W':
+                if (game->snake.dir != DOWN) game->snake.dir = UP;
+                break;
+            case 's': case 'S':
+                if (game->snake.dir != UP) game->snake.dir = DOWN;
+                break;
+            case 'a': case 'A':
+                if (game->snake.dir != RIGHT) game->snake.dir = LEFT;
+                break;
+            case 'd': case 'D':
+                if (game->snake.dir != LEFT) game->snake.dir = RIGHT;
+                break;
+            case 'q': case 'Q':
+            case 'x': case 'X':
+                game->gameOver = 1;
+                break;
+        }
+    }
+}
+
+void updateSnake(Game *game) {
+    int prevX = game->snake.x[game->snake.length - 1];
+    int prevY = game->snake.y[game->snake.length - 1];
+    
+    for (int i = game->snake.length - 1; i > 0; i--) {
+        game->snake.x[i] = game->snake.x[i - 1];
+        game->snake.y[i] = game->snake.y[i - 1];
+    }
+    
+    switch (game->snake.dir) {
+        case UP:    game->snake.y[0]--; break;
+        case DOWN:  game->snake.y[0]++; break;
+        case LEFT:  game->snake.x[0]--; break;
+        case RIGHT: game->snake.x[0]++; break;
+        default: break;
+    }
+    
+    gotoxy(prevX, prevY);
+    printf(" ");
+}
+
+void checkCollision(Game *game) {
+    if (game->snake.x[0] <= 0 || game->snake.x[0] >= WIDTH - 1 ||
+        game->snake.y[0] <= 0 || game->snake.y[0] >= HEIGHT - 1) {
+        game->gameOver = 1;
+        return;
+    }
+    
+    for (int i = 1; i < game->snake.length; i++) {
+        if (game->snake.x[0] == game->snake.x[i] && 
+            game->snake.y[0] == game->snake.y[i]) {
+            game->gameOver = 1;
+            return;
+        }
+    }
+    
+    if (game->snake.x[0] == game->food.x && game->snake.y[0] == game->food.y) {
+        game->snake.length++;
+        game->score += 10;
+        
+        do {
+            game->food.x = rand() % (WIDTH - 2) + 1;
+            game->food.y = rand() % (HEIGHT - 2) + 1;
+        } while (game->food.x == game->snake.x[0] && game->food.y == game->snake.y[0]);
+        
+        if (game->speed > 30) game->speed -= 2;
+    }
+}
+
+void showGameOver(Game *game) {
+    clearScreen();
+    setColor(31); // Red
+    printf("\n\n");
+    printf("    ╔═══════════════════════════════╗\n");
+    printf("    ║        GAME OVER!             ║\n");
+    printf("    ╚═══════════════════════════════╝\n\n");
+    
+    setColor(33); // Yellow
+    printf("    Final Score: %d\n", game->score);
+    printf("    Snake Length: %d\n\n", game->snake.length);
+    
+    setColor(37); // White
+    printf("    Thanks for playing!\n");
+    printf("    Press any key to exit...");
+    fflush(stdout);
+    
+    getchar();
+}
+
+int main() {
+    Game game;
+    
+    enableRawMode();
+    clearScreen();
+    
+    initGame(&game);
+    drawBorder();
+    
+    while (!game.gameOver) {
+        drawUI(&game);
+        drawFood(&game.food);
+        drawSnake(&game.snake);
+        
+        handleInput(&game);
+        updateSnake(&game);
+        checkCollision(&game);
+        
+        usleep(game.speed * 1000);
+        fflush(stdout);
+    }
+    
+    showGameOver(&game);
+    setColor(0);
+    printf("\033[0m\n");
+    
+    return 0;
+}
