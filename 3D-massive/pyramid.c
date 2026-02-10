cdfa48c7559d8bf06d46d6647d2f4cddc0349956
Akram <akrommuhammadyusuf@gmail.com>
2026-02-11 00:04:02 +0700

    Update pyramid.c

--- 3D-massive/pyramid.c
+++ 3D-massive/pyramid.c
@@ -2,3 +2,16 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <stdio.h>
+
+
+float angle = 0.0f;
+
+void drawPyramid() {
+    glBegin(GL_TRIANGLES);
+    
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f( 0.0f,  1.0f,  0.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);
+    glVertex3f( 1.0f, -1.0f,  1.0f);
+    
+
