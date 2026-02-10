e27773ca5cc7132703d2c0c228d72797505442b5
Akram <akrommuhammadyusuf@gmail.com>
2026-02-11 00:05:02 +0700

    Update pyramid.c

--- 3D-massive/pyramid.c
+++ 3D-massive/pyramid.c
@@ -29,3 +29,36 @@ void drawPyramid() {
     glVertex3f(-1.0f, -1.0f, -1.0f);
     glVertex3f(-1.0f, -1.0f,  1.0f);
     
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);
+    glVertex3f( 1.0f, -1.0f,  1.0f);
+    glVertex3f( 1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glEnd();
+    }
+    
+    void display() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glLoadIdentity();
+        
+        glTranslatef(0.0f, 0.0f, -6.0f);
+        glRotatef(angle, 0.0f, 1.0f, 0.0f);
+        glRotatef(angle * 0.5f, 1.0f, 0.0f, 0.0f);
+        
+        drawPyramid();
+        
+        glutSwapBuffers();
+    }
+    
+    void update(int value) {
+        angle += 2.0f;
+        if (angle > 360) angle -= 360;
+        
+        glutPostRedisplay();
+        glutTimerFunc(16, update, 0);
+    }
+    
+    
