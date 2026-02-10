b4ac4cd32942b2baaf3f29c965d574cd65b58ca2
Akram <akrommuhammadyusuf@gmail.com>
2026-02-11 00:04:30 +0700

    Update pyramid.c

--- 3D-massive/pyramid.c
+++ 3D-massive/pyramid.c
@@ -13,5 +13,19 @@ void drawPyramid() {
     glVertex3f( 0.0f,  1.0f,  0.0f);
     glVertex3f(-1.0f, -1.0f,  1.0f);
     glVertex3f( 1.0f, -1.0f,  1.0f);
-    
 
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f( 0.0f,  1.0f,  0.0f);
+    glVertex3f( 1.0f, -1.0f,  1.0f);
+    glVertex3f( 1.0f, -1.0f, -1.0f);
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f( 0.0f,  1.0f,  0.0f);
+    glVertex3f( 1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glVertex3f( 0.0f,  1.0f,  0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);
+    
